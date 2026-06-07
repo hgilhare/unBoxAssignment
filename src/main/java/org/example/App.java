fb0dc748e9a483c6e9ab51a0615296a1af74a2bb
Himanshu Gilhare <124993010+hgilhare@users.noreply.github.com>
2026-06-08 01:29:15 +0530

    Add Maven execution block to App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+                            <execution>
+                        <goals>
+                            <goal>prepare-agent</goal>
+                        </goals>
+                    </execution>
+
     }
 }
